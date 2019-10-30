ff9e04ab5ec0c18de0a5e787f9f1aef102993d4c
Emmanuel Cruz <emmanuelcv@gmail.com>
2019-10-30 15:15:07 -0600

    updated functions

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # gologger
 
-Simple webapp that when receives a GET request to path "/" displays the port on which the app is running and if it receives a GET request to path "/logger" display random transactional data.
+gologger - Simple webserver for testing purposes.
+When the server receives a GET request to path "/" displays the port on which the app is running.
+When the server receives a GET request to path "/logger" displays random transactional data.
 
 Usage of gologger:
   
@@ -10,17 +12,19 @@ Usage of gologger:
     -p int
         Port (default 8080)
 
+    -v  Display version information
+
 Example:
 
     loggerapp -a 192.168.99.78 -p 9090
 
     2019/08/15 17:22:05 Server started...
 
-Transactional log file fields are separated by pipes:
+Transactional data fields are separated by pipes:
     
     datetime|id|user|operation|duration|status|code|codeDescription
 
-Content example:
+Example:
 
     2019-08-07 10:10:51.223|1565190651223457650|ubbo-sathla|changeUser|548|SUCCESS|0|SUCCESS
     2019-08-07 10:10:51.385|1565190651385873762|nyarlathotep|cancelProduct|1214|FAILED|440|Not Supported
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -15,19 +16,41 @@ const (
 	defaultPath = "/"
 	loggerPath  = "/logger"
 	method      = "GET"
+	//By sets creator
+	By = "attilael1"
+	//Description is a simple description of the tool
+	Description = `gologger - Simple webapp for testing purposes.
+	If it receives a GET request to path "/" displays the port on which the app is running.
+	If it receives a GET request to path "/logger" display random transactional data.
+`
 )
 
 //flags of the app
 var (
 	address, path *string
 	port          *int
-	verbose       *bool
+	version       *bool
+	//Version sets app version
+	Version string
+	//GitCommit sets commit info
+	GitCommit string
+	//GitBranch sets git branch
+	GitBranch string
+	//BuildDate sets build date
+	BuildDate string
 )
 
-func main() {
+func init() {
 	address = flag.String("a", "localhost", "Hostname/IP address")
 	port = flag.Int("p", 8080, "Port")
+	version = flag.Bool("v", false, "Display version information")
 	flag.Parse()
+}
+
+func main() {
+	if *version == true {
+		ShowVersion()
+	}
 
 	bind := fmt.Sprintf("%v:%v", *address, *port)
 	http.HandleFunc(defaultPath, runApp)
@@ -39,21 +62,29 @@ func main() {
 }
 
 //runApp displays port of server
-func runApp(w http.ResponseWriter, req *http.Request) {
-	if req.URL.Path != defaultPath || req.Method != method {
-		http.NotFound(w, req)
+func runApp(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	if r.Method != method {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 	fmt.Fprintln(w, "Running App On Port", *port)
 }
 
 //loggerApp starts logging data to file
-func loggerApp(w http.ResponseWriter, req *http.Request) {
-	if req.URL.Path != loggerPath || req.Method != method {
-		http.NotFound(w, req)
+func loggerApp(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	if r.Method != method {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
+	fmt.Fprintf(w, "%v\n", Trans())
+}
 
+//ShowVersion shows app version
+func ShowVersion() {
+	fmt.Printf("Version: %v, Commit: %v-%v, Build: %v, By: %v\n", Version, GitCommit, GitBranch, BuildDate, By)
+	os.Exit(0)
 }
 
 //checkError takes a string and an error to log an error message
@@ -72,9 +103,9 @@ func Trans() string {
 	codes := []int{101, 105, 108, 140, 155, 1205, 666, 510, 419, 440}
 	messages := []string{"Unauthorized", "Service Not Found", "No Response From Server", "System Error", "Bad Request", "Forbidden", "Authentication Required", "Service Unavailable", "Timeout", "Not Supported"}
 
+	//Set Success response ratio to 60%
 	p := rand.Intn(10)
-	if p >= 7 {
-		status = "FAILED"
+	if p > 6 {
 		rc := rand.Intn(len(codes))
 		code = codes[rc]
 		msg = messages[rc]
@@ -91,6 +122,6 @@ func Trans() string {
 
 	timestamp := (time.Now().Local().Format("2006-01-02 15:04:05.000"))
 	tid := time.Now().UnixNano()
-	s := fmt.Sprintf("%v|%v|%v|%v|%v|%v|%v|%v\n", timestamp, tid, user, operation, duration, status, code, msg)
+	s := fmt.Sprintf("%v|%v|%v|%v|%v|%v|%v|%v", timestamp, tid, user, operation, duration, status, code, msg)
 	return s
 }
